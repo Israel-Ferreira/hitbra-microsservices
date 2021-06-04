8b4bf5c29aaebcd9da9de9d7f2a329b581c63231
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-06-04 14:04:04 -0300

    Eureka Configured

---
+++ eurekaserver/src/docker/run.sh
@@ -0,0 +1,1 @@
+java -Djava.security.egd=file:/dev/./urandom -jar /usr/local/eurekaserver/@project.build.finalName@.jar
---
+++ eurekaserver/src/test/java/com/hitbra/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitbra.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
